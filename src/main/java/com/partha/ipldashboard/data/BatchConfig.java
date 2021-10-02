0211e8863a559b15fdc34d341f5185e929b298c1
prathap <prathapgowda09@gmail.com>
2021-10-03 00:16:52 +0530

    some update

--- src/main/java/com/partha/ipldashboard/data/BatchConfig.java
+++ src/main/java/com/partha/ipldashboard/data/BatchConfig.java
@@ -26,7 +26,6 @@ import com.partha.ipldashboard.model.Match;
 public class BatchConfig {
 	
 	private final String[] FIELD_NAMES = new String[] {
-			
 			"id","city","date",	"player_of_match","venue","neutral_venue","team1","team2","toss_winner","toss_decision","winner","result","result_margin","eliminator","method","umpire1","umpire2" 
 	};
 	
